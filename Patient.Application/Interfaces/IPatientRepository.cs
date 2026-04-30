ffd2b25314bb78ea3fed98c1fa5739e53877f985
Alexandra Perrin <alexandrarose1@live.com.au>
2026-04-30 14:58:18 +0200

    updates to db formatting etc.

---
+++ Patient.Application/Interfaces/IPatientRepository.cs
@@ -0,0 +1,9 @@
+﻿//using Patient.Domain.Entities;
+
+//public interface IPatientRepository
+//{
+//    List<Patient> GetAll();
+//    Patient GetById(int id);
+//    void Add(Patient patient);
+//    void Update(Patient patient);
+//}
